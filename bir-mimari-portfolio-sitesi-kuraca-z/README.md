7bc97116fff2fa08c27ad3bcf3849270fd30144b
tunahantayincii <ttunahan5553@gmail.com>
2026-06-15 11:35:47 +0300

    Delete bir-mimari-portfolio-sitesi-kuraca-z directory

--- bir-mimari-portfolio-sitesi-kuraca-z/README.md
+++
@@ -1,5 +0,0 @@
-# Tunahan Tayıncı Portfolio
-
-Mimarlık öğrencisi portfolyosu. Netlify üzerinde yayınlanmak, Netlify Functions ile güvenli yönetilmek ve Supabase üzerinden kalıcı içerik/görsel depolamak üzere hazırlanmıştır.
-
-Kurulum adımları için [NETLIFY-KURULUM.md](NETLIFY-KURULUM.md) dosyasını inceleyin.
--- bir-mimari-portfolio-sitesi-kuraca-z/netlify/functions/auth-session.js
+++
@@ -1,7 +0,0 @@
-const { getCookie, json, verifyToken } = require("./_auth");
-
-exports.handler = async (event) => {
-  if (event.httpMethod !== "GET") return json(405, { error: "Method not allowed" }, { Allow: "GET" });
-  if (!verifyToken(getCookie(event))) return json(401, { authenticated: false });
-  return json(200, { authenticated: true });
-};
