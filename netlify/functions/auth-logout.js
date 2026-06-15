28a820213306614cf6758f9d57d42c4cab6b611f
tunahantayincii <ttunahan5553@gmail.com>
2026-06-15 12:13:05 +0300

    Delete netlify directory

--- netlify/functions/auth-logout.js
+++
@@ -1,6 +0,0 @@
-const { json, sessionCookie } = require("./_auth");
-
-exports.handler = async (event) => {
-  if (event.httpMethod !== "POST") return json(405, { error: "Method not allowed" }, { Allow: "POST" });
-  return json(200, { authenticated: false }, { "Set-Cookie": sessionCookie("", 0) });
-};
